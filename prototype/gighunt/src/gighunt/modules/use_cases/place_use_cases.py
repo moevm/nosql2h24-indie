cbe0486f507ab898ebc165878783e093b1e42a38
YariKar <kyaroslav03@gmail.com>
2024-11-05 23:20:17 +0300

    db: add base edge use cases class

--- prototype/gighunt/src/gighunt/modules/use_cases/place_use_cases.py
+++ prototype/gighunt/src/gighunt/modules/use_cases/place_use_cases.py
@@ -1,7 +1,7 @@
 from arango.graph import Graph
 
 from gighunt.modules.clients.arangodb_client import ArangoDBClient
-from gighunt.modules.use_cases.base_vertex_use_case import BaseVertexUseCase
+from gighunt.modules.use_cases.base_vertex_use_cases import BaseVertexUseCase
 
 class PlaceUseCases(BaseVertexUseCase):
     pass
