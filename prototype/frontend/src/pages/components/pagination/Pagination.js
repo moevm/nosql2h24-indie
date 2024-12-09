bfb8e4fea131b82608a2f064068f2200d4cc7111
Starodubov Maxim <tofumaks@gmail.com>
2024-12-10 01:54:24 +0300

    frontend: customize pagination

--- prototype/frontend/src/pages/components/pagination/Pagination.js
+++ prototype/frontend/src/pages/components/pagination/Pagination.js
@@ -1,19 +1,20 @@
 import './Pagination.css';
 
 import Button from '@mui/material/Button';
+import { CustomButton } from '../CustomMuiComponents.js';
 
 export default function Pagination(props) {
 
     return <>
-        <div className='flex-column width-full' style={{gap: '20px'}}>
-            <div className='flex-row width-full flex-space'>
-                <Button variant='contained'
+        <div className='flex-column width-full' style={{gap: '20px', padding: '20px'}}>
+            <div className='flex-row width-full flex-space align-center'>
+                <CustomButton variant='contained'
                     onClick={props.onPreviousClick}
-                >Previous</Button>
-                <div>{props.pageNumber}</div>
-                <Button variant='contained'
+                >Назад</CustomButton>
+                <div style={{padding: '10px 10px 10px 10px', backgroundColor: 'var(--primary-color', color: 'var(--secondary-color)', borderRadius: '10px'}}>Страница {props.pageNumber}</div>
+                <CustomButton variant='contained'
                     onClick={props.onNextClick}
-                >Next</Button>
+                >Далее</CustomButton>
             </div>
             <div className='flex-column width-full' style={{gap: '10px'}}>
                 {
